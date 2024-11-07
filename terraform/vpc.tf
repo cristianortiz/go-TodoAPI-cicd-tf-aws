cfbc762f396847b16acb0a8955a8857dae0c74d1
cristianortiz <cristian.ortiznavia82@gmail.com>
2024-11-07 01:05:34 -0300

    [terraform] new folder structure

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,12 +1,11 @@
 module "vpc" {
     source = "terraform-aws-modules/vpc/aws"
     version = "~> 5.1"
-    name = var.vpc_name
-    cidr = var.vpc_cidr
-
-    azs = var.azs
-    private_subnets = var.private_subnets
-    public_subnets = var.public_subnets
+    name = "go-todo-api-vpc"
+    cidr =  "10.0.0.0/16"
+    azs = ["us-east-1a","us-east-1b","us-east-1c"]
+    private_subnets = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+    public_subnets = ["10.0.101.0/24","10.0.102.0/24","10.0.103.0/24"]
     #internet outbound anly access for private subnets
     enable_nat_gateway = true
     enable_vpn_gateway = false
