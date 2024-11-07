e75578abbd41166ac9c4762e58eaa26121f0471e
cristianortiz <cristian.ortiznavia82@gmail.com>
2024-11-07 01:23:29 -0300

    [terraform] tf files changes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
   backend "s3" {
     bucket = "todoapi-tfbackend-bkt"
     key = "develop/terraform.tfstate"
     region = "us-east-1"
+    dynamodb_table="tf-statelocking"
     
   }
 }
